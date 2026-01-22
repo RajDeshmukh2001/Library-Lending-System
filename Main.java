cffe2435c13ccd07a0132e1e11e877e67b99a0c0
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2026-01-22 14:48:20 +0530

    feat: initialise Main class

---
+++ Main.java
@@ -0,0 +1,8 @@
+package Library-Lending-System;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Library Lending System!");
+        
+    }
+}
