5fde82dd113030d153b62cac522ee2443958c886
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-01-22 14:53:40 +0530

    feat: add Book entity with availability

---
+++ Book.java
@@ -0,0 +1,37 @@
+public class Book {
+    private int id;
+    private String title;
+    private int availableCopies;
+
+    public Book(int id, String title, int copies) {
+        this.id = id;
+        this.title = title;
+        this.availableCopies = copies;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public boolean isAvailable() {
+        return availableCopies > 0;
+    }
+
+    public void borrowCopy() {
+        if (availableCopies > 0) {
+            availableCopies--;
+        }
+    }
+
+    public void returnCopy() {
+        availableCopies++;
+    }
+}
